2389876e5cccd9e431db234a7a3c281b7261e2d3
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-01-18 02:07:34 +0300

    Added Feature-Sliced Design architectural methodology

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,5 +1,12 @@
 import webpack from 'webpack';
+import { BuildOptions } from './types/config';
 
-export function buildResolvers(): webpack.ResolveOptions {
-  return { extensions: ['.tsx', '.ts', '.js'] };
+export function buildResolvers(options: BuildOptions): webpack.ResolveOptions {
+  return {
+    extensions: ['.tsx', '.ts', '.js'],
+    preferAbsolute: true, // Предпочитать абсолютный путь
+    modules: [options.paths.src, 'node_modules'], // Папки с которых будет применяться абсолютный путь
+    mainFiles: ['index'], // Указывает главный файл для модуля
+    alias: {}, // С какого символа будет начинаться абсолютный путь ({} - без начального символа)
+  };
 }
