17d34cf07f6059482d83ad20160c637f444b5ae0
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-01-17 22:34:47 +0300

    Initial commit

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo sequi, officia nam
+      minus eaque ipsam, quas odit perspiciatis natus molestiae est alias recusandae
+      ratione, magni quisquam quibusdam laudantium velit explicabo!
+    </div>
+  );
+};
+
+export default MainPage;
