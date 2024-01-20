c3621a58f84e9106a9d97149f253d9fbc5c5a6fd
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-01-20 17:28:24 +0300

    Add WHR webpack plugin

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,12 +2,7 @@ import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
   const { t } = useTranslation('main');
-  return (
-    <div>
-      {t('Главная страница')}
-      {t('Главная страница')}
-    </div>
-  );
+  return <div>{t('Main page')}</div>;
 };
 
 export default MainPage;
